eb32a4b3a995d32c08c35deffebcbd616887cba9
Aykut Önen <aykutonen@gmail.com>
2020-09-06 18:44:26 +0300

    Base service yapısı için interface oluşturuldu.

---
+++ Service/Base/IService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Base
+{
+    public interface IService<T> : ISave
+    {
+        ReturnModel<T> Create(T entity);
+        ReturnModel<T> Update(T entity);
+        ReturnModel<IEnumerable<T>> Get(long id);
+        ReturnModel<long> Delete(long id);
+    }
+}
